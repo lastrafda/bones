30d7b4b47ac89593485fbf306ed624101cca9880
José Lastra <53153650+lastrafda@users.noreply.github.com>
2022-08-24 23:08:13 -0400

    add urlencoded and json middlewares

--- source/app.ts
+++ source/app.ts
@@ -3,6 +3,10 @@ import express, { Express, Request, Response } from "express"
 import config from "config"
 const port: number = config.get("port")
 const app: Express = express()
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.get("/", (req: Request, res: Response) => {
   res.send(200)
